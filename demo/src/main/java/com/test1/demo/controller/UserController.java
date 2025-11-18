475f4a2843b529cb9fe1825e8b4ab1a828070b6e
saviinda <savi077nda@gmail.com>
2025-11-18 20:55:10 +0530

    database created and connected

---
+++ demo/src/main/java/com/test1/demo/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.test1.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value="api/v1")
+public class UserController {
+    @GetMapping("/getUser")
+    public String getUser() {
+        return "Hello User!";
+    }
+}
